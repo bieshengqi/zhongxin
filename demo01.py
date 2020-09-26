20c94da700203adac0ea8d44068f05c73e6e733d
Zhongxin <455183229@qq.com>
2020-09-26 11:27:23 +0800

    第一次提交代码

---
+++ demo01.py
@@ -0,0 +1,18 @@
+# """
+# print("ahuang")  #
+# print("哈哈哈"*100)
+# print("ahuang")
+# a = "张三"
+# print(a)
+
+# """
+# a = input("请输入：")
+# b = input("请输入：")
+# # print("input获取的内容：",a+b)
+# a ="花花喜欢豚豚"
+# b ="但是豚豚不喜欢"
+# print(a+b)
+# print(len(a+b))
+a = input("请输入:")
+b = input("请输入:")
+print("两段字符串的长度是：",len(a)+len(b))
