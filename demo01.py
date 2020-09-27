68f1ee1da162eb21cb8a83d37e9e1c11d6512d56
Zhongxin <455183229@qq.com>
2020-09-27 19:04:48 +0800

    字典

--- demo01.py
+++ demo01.py
@@ -1,5 +1,26 @@
 
+"""
+a = {"name":"花花","age":22,"like":"chil","motto":"星光不负赶路人"}
+print(a["name"])
 
-a = input("请输入:")
-b = input("请输入:")
-print("两段字符串的长度是：",len(a)+len(b))
+b=a.get("motto")
+print (b)
+a.update(age=23)
+print(a)
+"""
+a = {
+    "name":"none",
+    "age":"none",
+    "sex":"none"
+    }
+
+a.update(name=input("请输入姓名:"))
+a.update(age=input("请输入年龄:"))
+a.update(sex=input("请输入性别:"))
+"""
+a("name")=input("请输入姓名：")
+a("age")=input("请输入年龄：")
+a("sex")=input("请输入性别：")
+"""
+print("用户的信息为:",a)
+
